b599183076ef97f637ddb97c45dde50551e609db
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2024-10-25 09:15:10 +0600

    Banner Section Done.

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import banner from '../../assets/banner-main.png'
+import backgroundShadow from '../../assets/bg-shadow.png'
+const Banner = () => {
+    return (
+        <div style={{
+            backgroundImage: `url(${backgroundShadow})`,
+        }} className='w-11/12 mx-auto my-20 rounded-lg py-10 px-6  md:px-16 gap-6 bg-cover bg-gray-900 bg-center flex flex-col'>
+            <img className='w-60 mx-auto' src={banner} alt="" />
+            <div className="text-center">
+                <h2 className='font-bold text-2xl md:text-4xl text-white mb-4'>Assemble Your Ultimate Dream 11 Cricket Team</h2>
+                <p className='font-medium text-xl md:text-2xl text-white-01 mb-6'>Beyond Boundaries Beyond Limits</p>
+                <button className='bg-btn-yellow text-dark-02 font-bold py-3 px-4 rounded-xl hover:bg-lime-500  shadow-lg ring-1 ring-offset-4 ring-btn-yellow dark:ring-offset-slate-900'>Claim Free Credit</button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ export default {
       colors: {
         'dark-01': 'rgba(19, 19, 19, 0.7)',
         'dark-02': 'rgb(19, 19, 19)',
+        'white-01': 'rgba(255, 255, 255, 0.7)',
+        'btn-yellow': 'rgb(231, 254, 41)',
       },
     },
   },
