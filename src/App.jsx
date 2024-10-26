1a7da0e7eea1124b842c75cb2425a5efcdbb93c8
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2024-10-26 13:23:03 +0600

    Pop up Done.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Players from './components/Players/Players';
 import Selected from './components/Selected/Selected';
 import Newsletter from './components/Newsletter/Newsletter';
 import Footer from './components/Footer/Footer';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
@@ -19,21 +21,31 @@ function App() {
 
   const addCoin = () => {
     setCoin(coin + 10000000);
-    alert(`Coins: ${coin + 100000}`);
+    toast.success(`Congrats! 10000000 Credit have been added to your account`, {
+      style: { backgroundColor: 'blue', color: 'white' }, 
+    });
   };
 
   const addPlayer = (player) => {
+    if (selectedPlayers.length >= 6) {
+      toast.warn("Your squad is full.", {
+        style: { backgroundColor: 'red', color: 'white' },
+      } );
+      return;
+    }
     if (selectedPlayers.some(p => p.playerId === player.playerId)) {
-        alert("Player already added.");
+      toast.warn(`${player.name}is already in your squad`);
         return;
     }
     if (coin < player.biddingPrice) {
-        alert("Insufficient coins.");
+      toast.warn(`Not enough coin to buy ${player.name}`, {
+        style: { backgroundColor: 'red', color: 'white' },
+      });
         return;
     }
     setSelectedPlayers([...selectedPlayers, player]);
     setCoin(coin - player.biddingPrice);
-    alert("Player added.");
+    toast.success(`Congrats! ${player.name} have been added to your squad`);
 };
 
 
@@ -41,7 +53,7 @@ const removePlayer = (playerId) => {
   const playerToRemove = selectedPlayers.find(player => player.playerId === playerId);
   if (playerToRemove) {
     setSelectedPlayers(selectedPlayers.filter(player => player.playerId !== playerId));
-    alert(`${playerToRemove.name} removed from selection.`);
+    toast.info(`${playerToRemove.name} removed from selection.`);
   }
 };
 
@@ -52,6 +64,7 @@ const removePlayer = (playerId) => {
 
   return (
     <>
+    <ToastContainer position="top-center"></ToastContainer>
     <Header coin={coin}></Header>
     <Banner addCoin={addCoin}></Banner>
     <Players addPlayer={addPlayer} activeTab={activeTab} setActiveTab={setActiveTab} selectedPlayers={selectedPlayers}></Players>
