ee3c650758de42c02402e17ff45753d5cd06cedd
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2024-10-26 16:24:07 +0600

    Header Sticky Done.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,8 @@ import coin1 from '../../assets/coin.png'
 // import { FaBeer } from 'react-icons/fa';
 const Header = ({ coin }) => {
     return (
-        <div className="flex justify-between items-center w-11/12 mx-auto md:mt-12">
+        <div className="sticky top-0 left-0 right-0 bg-white shadow z-50">
+            <div className="flex justify-between items-center w-11/12 mx-auto md:mt-12">
             <img src={logo} alt="" />
             <div className='flex gap-2'>
                 <ul className='hidden md:flex items-center gap-2 font-normal text-dark-01'>
@@ -14,6 +15,7 @@ const Header = ({ coin }) => {
                 </ul>
                 <button className='btn font-semibold text-dark-02'>{coin} coin <img src={coin1} alt="" /></button>
             </div>
+        </div>
         </div>
     );
 };
